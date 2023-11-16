5990e0c59c902da184de94ec63323a79b2abddc9
best2025j <audugodstime95@gmail.com>
2023-11-16 14:30:01 +0100

    nav added

---
+++ components/buttons/ButtonDefault.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+"use client";
+
+export default function ButtonDefault({ label, className }) {
+  return (
+    <button
+      className={`text-[#00AFF0] font-500 rounded-full px-6 py-3 active:text-[#ebecee] shadow-none capitalize active:scale-100 ${className}`}
+    >
+      {label}
+    </button>
+  );
+}
---
+++ components/buttons/ButtonWhite.jsx
@@ -0,0 +1,10 @@
+"use client";
+export default function ButtonWhite({ label, className }) {
+  return (
+    <button
+      className={`text-white rounded-full px-6 py-2 active:text-[#264ac4] shadow-none capitalize bg-[#00AFF0] active:scale-100 ${className}`}
+    >
+      {label}
+    </button>
+  );
+}

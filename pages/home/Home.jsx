8f34ae7d5bc4fdc68cda0335e310db66fcf75c6a
best2025j <audugodstime95@gmail.com>
2023-11-16 13:41:24 +0100

    fix

--- pages/home/Home.jsx
+++ pages/home/Home.jsx
@@ -1,19 +1,19 @@
-import { CarouselCards } from "@/components/carouselSections/CarouselCards";
-import { Instructors } from "@/components/carouselSections/Instructors";
-import { Footer } from "@/components/layout/Footer";
-import { Sections } from "@/components/sections/Sections";
-import { Carousel } from "@/components/ui/Carousel";
-import { ContactUs } from "@/components/ui/ContactUs";
-import { Hero } from "@/components/ui/Hero";
-import { OutSponsors } from "@/components/ui/OutSponsors";
-import Testimony from "@/components/ui/Testimony";
 import React from "react";
-
+// import { CarouselCards } from "@/components/carouselSections/CarouselCards";
+// import { Instructors } from "@/components/carouselSections/Instructors";
+// import { Footer } from "@/components/layout/Footer";
+// import { Sections } from "@/components/sections/Sections";
+// import { Carousel } from "@/components/ui/Carousel";
+// import { ContactUs } from "@/components/ui/ContactUs";
+// import { Hero } from "@/components/ui/Hero";
+// import { OutSponsors } from "@/components/ui/OutSponsors";
+// import Testimony from "@/components/ui/Testimony";
 
 const Home = () => {
   return (
     <>
-      <Hero />
+      <h1>welcome gt</h1>
+      {/* <Hero />
       <Carousel />
       <OutSponsors />
       <CarouselCards />
@@ -21,7 +21,7 @@ const Home = () => {
       <ContactUs />
       <Instructors />
       <Testimony />
-      <Footer />
+      <Footer /> */}
     </>
   );
 };

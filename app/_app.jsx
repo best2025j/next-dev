0a7cdf2895512b389fe281dc056ebbe4cae8d5bb
best2025j <audugodstime95@gmail.com>
2023-11-16 08:04:09 +0100

    changes

--- app/_app.jsx
+++ app/_app.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <div>
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }

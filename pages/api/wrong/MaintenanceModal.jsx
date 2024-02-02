61f942a325a83cd3dfe06dcae08c68e63d2a287d
best2025j <audugodstime95@gmail.com>
2024-02-02 21:06:53 +0000

    cloned

--- pages/api/wrong/MaintenanceModal.jsx
+++ pages/api/wrong/MaintenanceModal.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function MaintenanceModal() {
   return (
-    <div className="w-full h-screen bg-slate-200 flex justify-center items-center mx-auto flex-col">
-      <div className="bg-white h-72 flex-col flex items-center rounded-2xl justify-center  w-[60%]">
+    <div className="w-full h-screen bg-slate-200 dark:bg-black flex justify-center items-center mx-auto flex-col">
+      <div className="bg-white dark:bg-slate-900 h-72 flex-col flex items-center rounded-2xl justify-center  w-[60%]">
         <h1 className="text-xl font-[700] capitalize">
           sorry site is under Maintenance !!!
         </h1>

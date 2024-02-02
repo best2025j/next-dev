69ed7b1eaa0a2fc4e8f13afc60dec1f94b18e81c
best2025j <audugodstime95@gmail.com>
2024-02-02 21:23:22 +0000

    fix

--- pages/api/wrong/MaintenanceModal.jsx
+++ pages/api/wrong/MaintenanceModal.jsx
@@ -1,16 +1,19 @@
+import PageWrapper from "@/pages/PageWrapper";
 import React from "react";
 
 export default function MaintenanceModal() {
   return (
-    <div className="w-full h-screen bg-slate-200 dark:bg-black flex justify-center items-center mx-auto flex-col">
-      <div className="bg-white dark:bg-slate-900 h-72 flex-col flex items-center rounded-2xl justify-center  w-[60%]">
-        <h1 className="text-xl font-[700] capitalize">
-          sorry site is under Maintenance !!!
-        </h1>
-        <p className="">
-          We apologize for the inconvenience. Please check back later.
-        </p>
+    <PageWrapper>
+      <div className="w-full h-screen bg-slate-200 flex justify-center items-center mx-auto flex-col">
+        <div className="bg-white h-72 flex-col flex items-center rounded-2xl justify-center  w-[60%]">
+          <h1 className="text-xl font-[700] capitalize">
+            sorry site is under Maintenance !!!
+          </h1>
+          <p className="">
+            We apologize for the inconvenience. Please check back later.
+          </p>
+        </div>
       </div>
-    </div>
+    </PageWrapper>
   );
 }

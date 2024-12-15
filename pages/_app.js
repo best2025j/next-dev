059b410ee75f49f99f0c142613f9827039dc3e72
Boniface Audu <audugodstime95@gmail.com>
2024-12-15 20:48:06 +0100

    chnages made

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,12 @@ import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import { useRouter } from "next/router";
 import Footer from "@/components/layout/Footer";
-import MaintenanceModal from "./api/wrong/MaintenanceModal";
-import "react-datepicker/dist/react-datepicker.css";
+// import "react-datepicker/dist/react-datepicker.css";
 
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  // const isUnderMaintenance = true;
-  // const isHomePage = router.pathname === "/";
+  
 
   return (
     <ThemeProvider attribute="class">
